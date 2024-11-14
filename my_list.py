575f2e96c3d9b2791ae6aba8878cb4380add5b01
Perry1233 <mensahsilas91@gmail.com>
2024-11-14 22:07:42 +0000

    Week 2 Assignment

---
+++ my_list.py
@@ -0,0 +1,59 @@
+#Question 1.
+
+my_list = []
+
+print(my_list)
+
+
+#Question 2.
+
+my_list = [10, 20, 30, 40]
+
+print(my_list)
+
+
+#Question 3.
+
+my_list = [10, 20, 30, 40]
+
+my_list.insert(1,15)
+
+print(my_list)
+
+
+#Question 4.
+
+my_list = [10, 15, 20, 30, 40]
+
+additional_list = [50, 60, 70]
+
+my_list.extend(additional_list)
+
+print(my_list)
+
+
+#Question 5.
+
+my_list = [10, 15, 20, 30, 40, 50, 60, 70]
+
+my_list.pop()
+
+print(my_list)
+
+
+#Question 6.
+
+my_list = [10, 15, 20, 30, 40, 50, 60]
+
+my_list.sort()
+
+print(my_list)
+
+
+#Question 7.
+
+my_list = [10, 15, 20, 30, 40, 50, 60]
+
+index_of_30 = my_list.index(30)
+
+print(index_of_30)
